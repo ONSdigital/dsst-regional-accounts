8f273bc1ad702061b6b79fcbaedd8149c2981af1
Murrell <Chloe.Murrell@ons.gov.uk>
2022-06-28 12:41:10 +0100

    changes to pipeline

--- pipeline.py
+++ pipeline.py
@@ -11,22 +11,34 @@ def load_config(yaml_path: str):
     with open(yaml_path, 'r') as f:
         return yaml.safe_load(f)
 
-config = load_config("config\config.yaml")
 
-def run(data, file)-> pd.DataFrame:
+def run(config_path)-> pd.DataFrame:
     """
 
     """
+    config = load_config(config_path)
 
-    df = pre_processing(data)
+    df_1 = load_data(config["input_data"]["input_path"], "P1", config["input_data"]["P1_delete_top_rows"])
+    df_2 = load_data(config["input_data"]["input_path"], "P2", config["input_data"]["P2_delete_top_rows"])
+
+    df_1 = pre_processing(df_1)
+    df_2 = pre_processing(df_2)
 
     start_year = config["timeseries"]["start_year"]
     end_year = config["timeseries"]["end_year"] + 1
 
     years = create_years(start_year, end_year)
 
-    df = P_calculation(df, years, file)
+    df_1 = P_calculation(df_1, years, "P.1")
+    df_2 = P_calculation(df_2, years, "P.2")
+
+    export_data(df_1, df_2, config["output_file"]["output_path"], config["output_file"]["sheet_1_name"], config["output_file"]["sheet_2_name"])
  
+    return df_1, df_2
+
+def load_data(input_path, sheet_name, delete_rows):
+    df = pd.read_excel(input_path, sheet_name=sheet_name, skiprows=delete_rows)
+
     return df
 
 def pre_processing(df):
@@ -64,15 +76,17 @@ def P_calculation(df, years, file):
     df.insert(0, "Transaction", file)
 
     return df
+
+def export_data(P1, P2, output_path, sheet_name_1, sheet_name_2):
+    with pd.ExcelWriter(output_path) as writer:
+        P1.to_excel(writer, sheet_name = sheet_name_1, index=False)
+        P2.to_excel(writer, sheet_name = sheet_name_2, index=False)
+    
+    return None
     
 if __name__=='__main__':
 
-    data_P1 = pd.read_excel(config["input_data"]["input_path"], sheet_name="P1", skiprows=config["input_data"]["P1_delete_top_rows"])
-    print(data_P1)
-    data_P2 = pd.read_excel(config["input_data"]["input_path"], sheet_name="P2", skiprows=config["input_data"]["P2_delete_top_rows"])
-    P1 = run(data_P1, "P.1")
-    P2 = run(data_P2, "P.2")
+    config_path = "config\config.yaml"
 
-with pd.ExcelWriter(config["output_file"]["output_path"],) as writer:
-        P1.to_excel(writer, sheet_name=config["output_file"]["sheet_1_name"], index=False)
-        P2.to_excel(writer, sheet_name=config["output_file"]["sheet_2_name"], index=False)
+    df = run(config_path)
+
